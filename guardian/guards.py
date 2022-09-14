5d2e849465db4e91eab5df79306eebf926742c5b
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2022-09-14 21:07:31 +0200

    Delete guards.py

--- guardian/guards.py
+++
@@ -1,8 +0,0 @@
-# Since guards is a package now this file should be deleted
-# Should Add type errors when using guard validation for the value type
-
-# Strings
-# Dicts ? contains ?
-# iterables
-# Dates
-# Classes
