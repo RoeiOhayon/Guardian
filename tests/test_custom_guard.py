89867f530b56059972202c1aea7a4f609bf7f3bf
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2022-09-23 03:26:19 +0300

    Fix Custom Guard Tests

--- tests/test_custom_guard.py
+++ tests/test_custom_guard.py
@@ -4,18 +4,18 @@ import pytest
 
 @pytest.fixture()
 def custom_guard():
-    return Guard("CustomGuard", lambda x: x["custom"] is None, "Custom Guard")
+    return Guard("CustomGuard", lambda x, r: x["custom"] == r, "Custom Guard")
 
 
 def test_custom_guard_raises(custom_guard):
     with pytest.raises(ValueError):
-        custom_guard({"custom": None})
+        custom_guard({"custom": True}, True)
 
 
 def test_decorator_custom_guard_raises(custom_guard):
-    @custom_guard()
+    @custom_guard(True)
     def function(a, b, c):
         return a or b or c
 
     with pytest.raises(ValueError):
-        function({"custom": True}, {"custom": True}, {"custom": None})
+        function({"custom": False}, {"custom": False}, {"custom": True})
