e99accd084b482025a1d58f4b3cac80d81e3f466
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2022-09-23 05:08:11 +0300

    Add publish files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='guardian-python',
+    packages=['guardian'],
+    version='1.0.0',
+    description='Guard Clause Library',
+    author='Roei Ohayon',
+    author_email='',
+    url='https://github.com/RoeiOhayon/Guardian',
+)
