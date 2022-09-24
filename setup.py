65db57991a799fe68514737b474e6ea4776353eb
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2022-09-24 19:39:53 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='guardian-python',
-    packages=['guardian'],
-    version='1.0.0',
+    packages=['guardian', 'guardian.atomic_guards'],
+    version='1.0.7',
     description='Guard Clause Library',
+    long_description="This library enables using guard clauses, and provides decorators for guarding functions",
     author='Roei Ohayon',
     author_email='',
     url='https://github.com/RoeiOhayon/Guardian',
