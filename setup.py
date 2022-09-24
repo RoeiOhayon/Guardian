08d0146edaac52f7f9bd960152792c4d44ff829d
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2022-09-25 00:46:59 +0300

    Update Package Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='guardian-python',
     packages=['guardian', 'guardian.atomic_guards'],
-    version='1.0.0',
+    version='0.0.0',
     description='Guard Clause Library',
     long_description="With this package, you can use guard clauses both as function decorators and general guard clauses.",
     author='Roei Ohayon',
