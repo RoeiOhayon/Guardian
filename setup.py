f4712da44f6cd4b9ac6704515467d5c490089ba6
RoeiOhayon <68480989+RoeiOhayon@users.noreply.github.com>
2024-08-24 20:02:09 +0300

    Update setup.py
    Fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Guard Clause Package',
     long_description="Guardian allows you to use guard clauses both as function decorators and regular guard clauses\n"
                      "Guard Clause Decorators allow you to check all of a function's arguments.\n"
-                     "Guardian also enable you to create your own custom guard clauses.\n\nLatest Version: 1.1.1\n"
+                     "Guardian also enable you to create your own custom guard clauses.\n\nLatest Version: 1.1.2\n"
                      "Come visit: https://github.com/RoeiOhayon/Guardian",
     author='Roei Ohayon',
     maintainer='Roei Ohayon',
